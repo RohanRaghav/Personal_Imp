296f48014b99bf2c1f40e2bd0f5b9c78a1a5f47d
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2023-09-27 21:06:47 +0530

    Update ProjectItem.js

--- ProjectItem.js
+++ ProjectItem.js
@@ -1,23 +1,43 @@
 import React, { Component } from "react";
 
 export default class ProjectItem extends Component {
+  truncateDescription(description, maxLength) {
+    if (description.length <= maxLength) {
+      return description;
+    } else {
+      return description.substring(0, maxLength) + "…";
+    }
+  }
+
   render() {
+    const { project } = this.props; // Destructure the project prop
+    const maxLength = 100; // Maximum length for description
+
+    // Define the fixed width and height for the image
+    const imageStyle = {
+      width: "207px", // Set the desired width
+      height: "175px", // Set the desired height
+    };
+
     return (
-      <div className="container my-3">
-        <div className="card" style={{ width: 210 }}>
-          <img src="/" className="card-img-top" alt="website" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
+      <div>
+      <div className="card my-4" style={{ width: 210 }}>
+        <img
+          src={project.photoLink}
+          className="card-img-top"
+          alt="website"
+          style={imageStyle} // Apply the fixed size to the image
+        />
+        <div className="card-body">
+          <h5 className="card-title">{project.label}</h5>
+          <p className="card-text">
+            {this.truncateDescription(project.description, maxLength)}
+          </p>
+          <a href="/" className="btn btn-primary">
+            Go somewhere
+          </a>
         </div>
-      </div>
+      </div></div>
     );
   }
 }
