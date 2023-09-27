b7b03ab156a84ad2dc9df7a2e54a322bbd7be273
Rohan Raghav <132052764+RohanRaghav@users.noreply.github.com>
2023-09-27 21:05:32 +0530

    Update MainPage.js

--- MainPage.js
+++ MainPage.js
@@ -1,24 +1,23 @@
 import React from "react";
 import Navbar from "./Component/Navbar";
-import ProjectForm from "./Component/ProjectForm";
 import Projects from "./Component/Projects";
 import CustomCursor from "./Component/CustomCursor";
 import { Route, Routes, Navigate } from 'react-router-dom';
-import AddProject from "./Component/AddProject";
+import "./MainPage.css";
+
 
 function MainPage() {
   return (
-    <div>
+    <div className="app-container">
         <Navbar />
         <CustomCursor /> 
-        <AddProject />{/* Render the custom cursor component */}
+        {/* Render the custom cursor component */}
           {/* Your content components here */}
           <Routes>
             <Route path="/" element={<Projects />} />
-            <Route path="/Addition" element={<ProjectForm />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
-    </div>
+    </div> 
   );
 }
 
